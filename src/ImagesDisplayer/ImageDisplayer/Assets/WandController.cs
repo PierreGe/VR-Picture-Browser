f3c30eecc568cda9aaec28d296aa2a3d4750863d
Bruno ROCHA PEREIRA <brochape@ulb.ac.be>
2016-12-14 20:19:06 +0100

    [Add] Go up and down

--- src/ImagesDisplayer/ImageDisplayer/Assets/WandController.cs
+++ src/ImagesDisplayer/ImageDisplayer/Assets/WandController.cs
@@ -7,6 +7,8 @@ public class WandController : MonoBehaviour {
     // Controller
 
     private Valve.VR.EVRButtonId gripButton = Valve.VR.EVRButtonId.k_EButton_Grip;
+    private Valve.VR.EVRButtonId upButton = Valve.VR.EVRButtonId.k_EButton_DPad_Up;
+    private Valve.VR.EVRButtonId downButton = Valve.VR.EVRButtonId.k_EButton_DPad_Down;
     private Valve.VR.EVRButtonId triggerButton = Valve.VR.EVRButtonId.k_EButton_SteamVR_Trigger;
 
     private SteamVR_Controller.Device controller { get { return SteamVR_Controller.Input((int)trackedObj.index); } }
@@ -18,9 +20,9 @@ public class WandController : MonoBehaviour {
 
         trackedObj = GetComponent<SteamVR_TrackedObject>();
     }
-	
-	// Update is called once per frame
-	void Update ()
+
+    // Update is called once per frame
+    void Update()
     {
         if (controller == null)
         {
@@ -29,11 +31,25 @@ public class WandController : MonoBehaviour {
         }
         if (controller.GetPressDown(triggerButton))
         {
-            Debug.Log("Pressed");
             GameObject selected = Main.scriptInstance.currentSelected;
             if (selected)
-                Main.scriptInstance.currentSelected.transform.Rotate(new Vector3(0,90,0));
+                Main.scriptInstance.currentSelected.transform.Rotate(new Vector3(0, 90, 0));
         }
+        if (controller.GetPress(SteamVR_Controller.ButtonMask.Touchpad))
+        {
+            Vector2 touchpad = (controller.GetAxis(Valve.VR.EVRButtonId.k_EButton_Axis0));
 
+            if (touchpad.y > 0.7f)
+            {
+                print("Moving Up");
+                GameObject.Find("CameraParent").transform.Translate(new Vector3(0, (float)0.3, 0));
+            }
+
+            else if (touchpad.y < -0.7f)
+            {
+                print("Moving Down");
+                GameObject.Find("CameraParent").transform.Translate(new Vector3(0, (float)-0.3, 0));
+            }
+        }
     }
 }
