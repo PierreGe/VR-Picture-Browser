eaf11ce13ea41664ffe21d3dd2f8ac00eaa67be3
Pierre Gérard <pierre.gerard84@gmail.com>
2016-12-14 12:41:23 +0100

    Closer and farther

--- src/ImagesDisplayer/ImageDisplayer/Assets/Main.cs
+++ src/ImagesDisplayer/ImageDisplayer/Assets/Main.cs
@@ -17,6 +17,8 @@ public class Main : MonoBehaviour
     bool waitForAnd = false;
     bool waitForOr = false;
 
+    float scale = 15;
+
     public static Main scriptInstance;
 
     String shownTags = "";
@@ -41,9 +43,11 @@ public class Main : MonoBehaviour
 	{
 
 		float sizex = theGameObject.GetComponent<Renderer>().bounds.size.x;
-		float sizey = theGameObject.GetComponent<Renderer>().bounds.size.y;
+        Debug.Log(sizex);
+        float sizey = theGameObject.GetComponent<Renderer>().bounds.size.y;
+        Debug.Log(sizey);
 
-		Vector3 rescale = theGameObject.transform.localScale;
+        Vector3 rescale = theGameObject.transform.localScale;
 
 		rescale.x = newSizex * rescale.x / sizex;
 		rescale.y = newSizey * rescale.y / sizey;
@@ -64,6 +68,31 @@ public class Main : MonoBehaviour
         waitForOr = true;
         shownTags += " OR ";
         showTag();
+    }
+
+    public void onSmallerRecognised()
+    {
+        scale = (float) 0.85 * scale;
+        loadPictures(currentPictures);
+    }
+
+    public void onBiggerRecognised()
+    {
+        scale = (float)1.15 * scale;
+        loadPictures(currentPictures);
+    }
+
+    public void onResetRecognised()
+    {
+        w.Clear();
+        foreach (GameObject o in planes)
+        {
+            Destroy(o);
+        }
+        planes.Clear();
+        scale = 15;
+        waitForAnd = false;
+        waitForOr = false;
     }
 
     private void loadTexture(string filename)
@@ -124,16 +153,19 @@ public class Main : MonoBehaviour
         planes.Clear();
 
 		float sqrt2 = Mathf.Sqrt (2)/2; 
-		float size = 20;
-		float[,] indexes = {
-			{0,size,90,0,0},
-			{size*sqrt2,size*sqrt2,90,45,0},
-			{-size*sqrt2,size*sqrt2, 90, -45, 0},
-			{size,0, 90, 90, 0},
-			{-size,0, 90, -90, 0},
-			{size*sqrt2,-size*sqrt2, 90, 135, 0},
-			{-size*sqrt2,-size*sqrt2, 90, -135, 0},
-			{0,-size,90,180,0}
+		float size = scale + (float) 0.4; // changed from 20
+        int magicnumber = 90;
+        int magicnumber2 = 45;
+        int magicnumber3 = 135;
+        float[,] indexes = {
+			{0,size,magicnumber,0,0},
+			{size*sqrt2,size*sqrt2,magicnumber,magicnumber2,0},
+			{-size*sqrt2,size*sqrt2, magicnumber, -magicnumber2, 0},
+			{size,0, magicnumber, magicnumber, 0},
+			{-size,0, magicnumber, -magicnumber, 0},
+			{size*sqrt2,-size*sqrt2, magicnumber, magicnumber3, 0},
+			{-size*sqrt2,-size*sqrt2, magicnumber, -magicnumber3, 0},
+			{0,-size,magicnumber,180,0}
 		};
 		index = 0;
 		int current = 0;
@@ -146,14 +178,14 @@ public class Main : MonoBehaviour
 				level ++;
 			}
 			float indexx = indexes [current,0];
-			float indexy = level*12 + 5;
+			float indexy = level* (scale - (float) 0.5) + (float) 5; // changed from *12 +5
 			float indexz = indexes [current, 1];
 
             planes.Add(GameObject.CreatePrimitive(PrimitiveType.Plane));
 			planes[index].transform.position = new Vector3(indexx, indexy, indexz);
 			planes [index].transform.Rotate (new Vector3 (indexes [current,2], 180 + indexes [current,3], indexes [current,4]));
             loadTexture(picture.getPath());
-            indexx += 20;
+            indexx += size;
 			current++;
 			index++;
         }
