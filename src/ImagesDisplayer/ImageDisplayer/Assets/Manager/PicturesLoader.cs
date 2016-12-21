01be854bc4342199451716591ae3762f27414921
Matteo Marra <mmarra@vub.ac.be>
2016-12-22 00:12:38 +0100

    commented code and removed querier (not used)

--- src/ImagesDisplayer/ImageDisplayer/Assets/Manager/PicturesLoader.cs
+++ src/ImagesDisplayer/ImageDisplayer/Assets/Manager/PicturesLoader.cs
@@ -7,40 +7,51 @@ namespace Assets
 {
     public class PicturesLoader
     {
+        //lower confidence level accepted
         private const double LIMIT = 0.35;
 
         public static PicturesManager parseJson(string path)
         {
+            /*
+             * Read the Json file and create it in the object model of the Library.
+             * The file contains a list of pairs:
+             * {image_path, [{score,tag}]}
+             * */
             string jsonFile = File.ReadAllText(path);
             JObject obj = JObject.Parse(jsonFile);
             JToken currentObj = obj.First;
+            //initialize a manager
             PicturesManager picturesManager = new PicturesManager();
+            //iterate the picture
             while (currentObj != null)
             {
                 JProperty currentProperty = (JProperty)currentObj;
                 string name = currentProperty.Name;
                 JToken values = currentProperty.Value;
+                //iterate the {score,tag} list
                 foreach (JToken value in values)
                 {
                     JProperty score = (JProperty)value.First;
                     JProperty tags = (JProperty)value.Last;
+                    //filter the tags for their score
                     if ((double)score.Value > LIMIT)
                     {
+                        //separate the tags
                         String selectedTags = (String)tags.First;
                         String[] splitted = selectedTags.Split(',');
                         for(int i = 0; i < splitted.Length; i++)
                         {
                             splitted[i] = splitted[i].Trim();
                         }
+                        //add the picture in the manager
                         picturesManager.addPicture(name, splitted);
-                        /*foreach (String s in splitted)
-                        {
-                            picturesManager.addPictureForTag(name, s.Trim());
-                        }*/
+                        
                     }
                 }
+                //update the currentobject
                 currentObj = currentObj.Next;
             }
+            //return the manager
             return picturesManager;
         }
 
